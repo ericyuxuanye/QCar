5850ae31d52806d963f4e7f1ed90ac40161f7708
Eric Ye <yuxuan.smiley@gmail.com>
2023-03-01 12:21:39 -0800

    Improve model by using more cones

--- game.py
+++ game.py
@@ -7,8 +7,6 @@ from sprites import Car, TrafficConePair
 
 from model import Model
 
-model = Model()
-
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -16,6 +14,9 @@ SCREEN_HEIGHT = 600
 HUMAN = False
 
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+if not HUMAN:
+    model = Model()
 
 done = False
 
@@ -31,12 +32,14 @@ last_cone = len(traffic_cones.sprites()) - 1
 x = 10
 y = 10
 
+
 def action_to_key(action):
     if action == 0:
         return {K_LEFT: True, K_RIGHT: False}
     elif action == 1:
         return {K_LEFT: False, K_RIGHT: True}
     return {K_LEFT: False, K_RIGHT: False}
+
 
 counter = 0
 
@@ -45,16 +48,35 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
-    collided_cone = pygame.sprite.spritecollideany(car, traffic_cones, pygame.sprite.collide_mask)
-    
+    collided_cone = pygame.sprite.spritecollideany(
+        car, traffic_cones, pygame.sprite.collide_mask
+    )
+
     if HUMAN:
         pressed = pygame.key.get_pressed()
     else:
         if counter % 10 == 0:
-            car_cone1 = last_cone - 2 if last_cone > 1 else len(traffic_cones.sprites()) - 1
-            car_cone2 = last_cone - 3 if last_cone > 2 else len(traffic_cones.sprites()) - 1
-            model.update(-1 if collided_cone else 1)
-            action = model.get_action(car.rect.left - (traffic_cones.sprites()[car_cone1].rect.left + 50), car.rect.left - (traffic_cones.sprites()[car_cone2].rect.left + 50))
+            car_cone1 = (
+                last_cone - 1
+                if last_cone > 0
+                else len(traffic_cones.sprites()) - 1 + last_cone
+            )
+            car_cone2 = (
+                last_cone - 2
+                if last_cone > 1
+                else len(traffic_cones.sprites()) - 2 + last_cone
+            )
+            car_cone3 = (
+                last_cone - 3
+                if last_cone > 2
+                else len(traffic_cones.sprites()) - 3 + last_cone
+            )
+            model.update(-1 if collided_cone else 2)
+            action = model.get_action(
+                car.rect.left - (traffic_cones.sprites()[car_cone1].rect.left + 50),
+                car.rect.left - (traffic_cones.sprites()[car_cone2].rect.left + 50),
+                car.rect.left - (traffic_cones.sprites()[car_cone3].rect.left + 50),
+            )
             pressed = action_to_key(action)
 
     restrict_left = False
@@ -64,7 +86,7 @@ while not done:
             restrict_left = True
         else:
             restrict_right = True
-    
+
     car.update(pressed, restrict_left, restrict_right)
 
     screen.fill((128, 128, 128))
@@ -76,10 +98,16 @@ while not done:
         for cone in traffic_cones:
             cone.update()
         if traffic_cones.sprites()[last_cone].out_of_bounds():
-            first_cone = last_cone + 1 if last_cone < len(traffic_cones.sprites()) - 1 else 0
-            traffic_cones.sprites()[last_cone].reload(traffic_cones.sprites()[first_cone].rect.x)
-            last_cone = last_cone - 1 if last_cone > 0 else len(traffic_cones.sprites()) - 1
-                
+            first_cone = (
+                last_cone + 1 if last_cone < len(traffic_cones.sprites()) - 1 else 0
+            )
+            traffic_cones.sprites()[last_cone].reload(
+                traffic_cones.sprites()[first_cone].rect.x
+            )
+            last_cone = (
+                last_cone - 1 if last_cone > 0 else len(traffic_cones.sprites()) - 1
+            )
+
     clock.tick(60)
     counter += 1
 
--- model.py
+++ model.py
@@ -1,18 +1,21 @@
 import random
 
+
 class Model:
     def __init__(self):
         # Initialize the qtable
         # the state has 3 values: the distance to the closest cone, the distance to the second closest cone, and the distance to the third closest cone
         # first action is left, second is right, third is nothing
-        self.qtable = [[[0.0, 0.0, 0.0] for i in range(15)] for j in range(15)]
+        self.qtable = [
+            [[[0.0, 0.0, 0.0] for i in range(15)] for j in range(15)] for k in range(15)
+        ]
         self.alpha = 0.1
         self.gamma = 0.9
-        self.epsilon = 0.2
+        self.epsilon = 0.1
         self.last_state = None
         self.last_action = None
         self.last_reward = None
-        self.state = [7, 7]
+        self.state = [7, 7, 7]
         self.action = 2
 
         # if we have a qtable, load it
@@ -20,32 +23,47 @@ class Model:
             with open("qtable.txt", "r") as f:
                 for i in range(15):
                     for j in range(15):
-                        self.qtable[i][j] = [float(x) for x in f.readline().split()]
+                        for k in range(15):
+                            self.qtable[i][j][k] = [
+                                float(x) for x in f.readline().split()
+                            ]
         except:
             pass
-        
-    def get_action(self, state1, state2):
+
+    def get_action(self, state1, state2, state3):
         # state should be the distance from left cone, from 0 to 200
         # We also leave a margin of 200 pixels on each side
         # Then we divide by 40 to get the state
-        self.state = [(state1 - 200)//40, (state2 - 200)//40]
+        self.state = [(state1 + 200) // 40, (state2 + 200) // 40, (state3 + 200) // 40]
         # epsilon greedy
         if random.random() < self.epsilon:
             self.action = random.randint(0, 2)
             return self.action
         else:
-            self.action = self.qtable[self.state[0]][self.state[1]].index(max(self.qtable[self.state[0]][self.state[1]]))
+            self.action = self.qtable[self.state[0]][self.state[1]][
+                self.state[2]
+            ].index(max(self.qtable[self.state[0]][self.state[1]][self.state[2]]))
             return self.action
-        
+
     def update(self, reward):
         if self.last_state is not None:
-            self.qtable[self.last_state[0]][self.last_state[1]][self.last_action] += self.alpha * (reward + self.gamma * max(self.qtable[self.state[0]][self.state[1]]) - self.qtable[self.last_state[0]][self.last_state[1]][self.last_action])
+            self.qtable[self.last_state[0]][self.last_state[1]][self.last_state[2]][
+                self.last_action
+            ] += self.alpha * (
+                reward
+                + self.gamma
+                * max(self.qtable[self.state[0]][self.state[1]][self.state[2]])
+                - self.qtable[self.last_state[0]][self.last_state[1]][
+                    self.last_state[2]
+                ][self.last_action]
+            )
         self.last_state = self.state
         self.last_action = self.action
         self.last_reward = reward
-    
+
     def save_qtable(self):
         with open("qtable.txt", "w") as f:
             for i in range(15):
                 for j in range(15):
-                    f.write(" ".join([str(x) for x in self.qtable[i][j]]) + "\n")
+                    for k in range(15):
+                        f.write(" ".join([str(x) for x in self.qtable[i][j][k]]) + "\n")
